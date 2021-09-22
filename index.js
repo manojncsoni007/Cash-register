a10fd300f6bfd4d74af0d88d88775a8e6233a232
Manoj Soni <manojncsoni@gmail.com>
2021-09-22 22:06:13 +0530

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('connected');
+let checkBtn = document.querySelector("#check-btn");
+let billAmount = document.querySelector("#bill-amount");
+let givenCash = document.querySelector("#given-cash");
+let message = document.querySelector("#error-msg");
+const noOfNotes = document.querySelectorAll('.no-of-notes');
+const notesAvailable = [2000, 500, 100, 20, 10, 5, 1];
+const nextButton = document.querySelector('#next-btn');
+let hidden = document.querySelectorAll('.hide');
+
+nextButton.addEventListener('click', () => {
+    hidden.style.visibility = "visible";
+
+
+})
+
+checkBtn.addEventListener('click', () => {
+    hide(message);
+    if (billAmount.value > 0) {
+        if (givenCash.value >= billAmount.value) {
+            let returnAmount = givenCash.value - billAmount.value;
+            calculateChange(returnAmount);
+        } else {
+            showMessage('Give cash atleast equal of amount');
+        }
+
+    } else {
+        showMessage('Amount Invalid')
+    }
+
+})
+
+function calculateChange(amount) {
+    for (let i = 0; i < notesAvailable.length; i++) {
+        const numberOfNotes = Math.trunc(amount / notesAvailable[i])
+        noOfNotes[i].innerText = numberOfNotes;
+        amount = amount % notesAvailable[i];
+    }
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
+
+function hide(param) {
+    param.style.display = 'none';
+}
